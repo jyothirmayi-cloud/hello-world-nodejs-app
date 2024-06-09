0c5e0e580512ccf959155554ca5d9bb3da09247e
jyothirmayi-cloud <57661206+jyothirmayi-cloud@users.noreply.github.com>
2024-06-09 20:56:29 +0530

    Update ecs_service.tf

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -1,15 +1,14 @@
 resource "aws_ecs_task_definition" "hello_world" {
-  family                   = "hello-world-task"
-  network_mode             = "awsvpc"
+  family                = "hello-world"
+  network_mode          = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-  memory                   = "512"
-  cpu                      = "256"
-
+  execution_role_arn    = aws_iam_role.ecs_task_execution.arn
+  cpu                   = "256"
+  memory                = "512"
   container_definitions = jsonencode([
     {
-      name = "hello-world"
-      image = "${aws_ecr_repository.hello_world.repository_url}:latest"
+      name      = "hello-world"
+      image     = "${aws_ecr_repository.hello_world[0].repository_url}:latest"
       essential = true
       portMappings = [
         {
@@ -19,18 +18,4 @@ resource "aws_ecs_task_definition" "hello_world" {
       ]
     }
   ])
-}
-
-resource "aws_ecs_service" "hello_world" {
-  name            = "hello-world-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.hello_world.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-  
-  network_configuration {
-    subnets          = aws_subnet.public[*].id
-    security_groups  = [aws_security_group.ecs_service.id]
-    assign_public_ip = true
-  }
 }
