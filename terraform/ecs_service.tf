ff9eebc5bec75d4505e0d9a96d2a3715e67e5767
jyothirmayi-cloud <57661206+jyothirmayi-cloud@users.noreply.github.com>
2024-06-10 16:27:38 +0530

    Update ecs_service.tf

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -1,15 +1,36 @@
-resource "aws_ecs_task_definition" "hello_world" {
-  family                   = "hello-world-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-  memory                   = "512"
-  cpu                      = "256"
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "hello_world" {
+  family                = "hello-world"
+  network_mode          = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn    = aws_iam_role.ecs_task_execution.arn
+  cpu                   = "256"
+  memory                = "512"
   container_definitions = jsonencode([
     {
-      name = "hello-world"
-      image = "${aws_ecr_repository.hello_world.repository_url}:latest"
+      name      = "hello-world"
+      image     = "${aws_ecr_repository.hello_world[0].repository_url}:latest"
       essential = true
       portMappings = [
         {
