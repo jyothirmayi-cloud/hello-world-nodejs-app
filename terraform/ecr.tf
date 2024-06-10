508d8bf4af5075d472395a34e613c2c68bff46a1
jyothirmayi-cloud <57661206+jyothirmayi-cloud@users.noreply.github.com>
2024-06-10 17:51:14 +0530

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,11 @@
-resource "aws_ecr_repository" "hello_world" {
+data "aws_ecr_repository" "hello_world" {
   name = "hello-world"
 }
+resource "aws_ecr_repository" "hello_world" {
+  count = length(data.aws_ecr_repository.hello_world.id) == 0 ? 1 : 0
+  name  = "hello-world"
+}
+
 
 
 
