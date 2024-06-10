ec864ebc0e83d068698a0017081f6dc985aef06a
jyothirmayi-cloud <57661206+jyothirmayi-cloud@users.noreply.github.com>
2024-06-10 18:06:18 +0530

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -5,7 +5,11 @@ resource "aws_ecr_repository" "hello_world" {
   count = length(data.aws_ecr_repository.hello_world.id) == 0 ? 1 : 0
   name  = "hello-world"
 }
-
+locals {
+  repository_url = length(aws_ecr_repository.hello_world) > 0 ?
+    aws_ecr_repository.hello_world[0].repository_url :
+    data.aws_ecr_repository.hello_world.repository_url
+}
 
 
 
