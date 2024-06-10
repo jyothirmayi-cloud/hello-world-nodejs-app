64bb24746138fbe020e0c4352c183e87541c85a7
jyothirmayi-cloud <57661206+jyothirmayi-cloud@users.noreply.github.com>
2024-06-10 17:39:02 +0530

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,3 +1,20 @@
+resource "aws_security_group" "allow_http" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_security_group" "ecs_service" {
   name        = "ecs_service_sg"
   description = "Allow traffic to ECS service"
