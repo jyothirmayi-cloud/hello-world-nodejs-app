773dfd7c75dddd8336f79df2939383db82a417ff
jyothirmayi-cloud <57661206+jyothirmayi-cloud@users.noreply.github.com>
2024-06-10 16:36:27 +0530

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,12 +1,6 @@
-# Data source to check if the ECR repository already exists
-data "aws_ecr_repository" "hello_world" {
+resource "aws_ecr_repository" "hello_world" {
   name = "hello-world"
-  # This will return the repository if it exists
 }
 
-resource "aws_ecr_repository" "hello_world" {
-  count = length(data.aws_ecr_repository.hello_world.repository_url) == 0 ? 1 : 0
-  name  = "hello-world"
-}
 
 
