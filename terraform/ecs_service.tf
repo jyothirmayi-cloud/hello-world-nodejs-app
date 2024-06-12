537818a61cefdff5d5f5ca77b87b14223fa43e8c
jyothirmayi-cloud <57661206+jyothirmayi-cloud@users.noreply.github.com>
2024-06-12 17:31:05 +0530

    Update ecs_service.tf

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -5,6 +5,7 @@ resource "aws_ecs_task_definition" "hello_world" {
   execution_role_arn   = aws_iam_role.ecs_task_execution.arn
   cpu                  = "256"
   memory               = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
     {
